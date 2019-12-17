a11a5abda97b79c6254c36fbad2a6328f139e1ec
humeinste <humeincreations@gmail.com>
2019-12-17 16:35:15 +0000

    FIX: format and comments

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,11 +1,14 @@
 --CREATE DATABASE - USER - PASSWORD - ASSIGN PRIVILEGES - FLUSH
-
+-- db = hbnb_dev_db
+-- new user hbnb_dev @ localhost
+-- hbnb_dev password = hbnb_dev_pwd
+-- all priv in db and select priv perform schema
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 
-GRANT ALL PRIVILEGES ON hbnb_dev_db TO 'hbnb_dev'@'localhost';
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
 
-GRANT SELECT ON performance_schema TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
 
 FLUSH PRIVILEGES;
--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,5 +1,7 @@
---CREATE DATABASE - USER - PASSWORD - ASSIGN PRIVILEGES - FLUSH                                
-
+--CREATE DATABASE - USER - PASSWORD - ASSIGN PRIVILEGES - FLUSH                               -- db = hbnb_test_db 
+-- user - hbnb_test @ localhost
+-- hbnb_test password = hbnb_test_pwd
+-- hbnb_test all priv in db hbnb_test_db && select priv on performance_schema
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
