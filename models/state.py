cb5daa26fff358a1a48bb5970baaf3a3620d139c
sranciato <sranciato1@gmail.com>
2019-12-18 18:35:34 +0000

    [ADD] state

--- models/state.py
+++ models/state.py
@@ -1,11 +1,25 @@
 #!/usr/bin/python3
 """This is the state class"""
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, Integer
+from sqlalchemy.orm import relationship
+from models.city import City
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """This is the class for State
     Attributes:
         name: input name
     """
-    name = ""
+    __tablename__ = "states"
+
+    name = Column(String(128), nullable=False)
+    cities = relationship("City", backref="state")
+
+    @property
+    city_list = []
+    c_dict = storage.all(City)
+    for c in c_dict.values():
+        if self.id == c.state_id:
+            city_list.append(c)
+    return city_list
