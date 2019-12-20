910f5eb0f9f455770ac45716c86df5b660d20d5c
sranciato <sranciato1@gmail.com>
2019-12-20 01:22:24 +0000

    [FIX] __tablename__ outside if statement

--- models/place.py
+++ models/place.py
@@ -20,8 +20,9 @@ class Place(BaseModel, Base):
         longitude: longitude in float
         amenity_ids: list of Amenity ids
     """
+    __tablename__ = "places"
+
     if os.getenv('HBNB_TYPE_STORAGE') == 'db':
-        __tablename__ = "places"
         city_id = Column(String(60), ForeignKey("cities.id"), nullable=False)
         user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
         name = Column(String(128), nullable=False)
