4696744ca84b3db58f647aa723e3b465ccc73d3c
humeinste <humeincreations@gmail.com>
2020-01-03 21:32:05 +0000

    checl

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -9,7 +9,7 @@ def do_pack():
     try:
         local("mkdir -p versions")
         time = dateime.now().strftime("%Y%m%d%H%M%S")
-        local("tar -cvsf versions/web_static_{}.tgz".format(time))
+        local("tar -cvzf versions/web_static_{}.tgz".format(time))
         return "versions/web_static_{}.tgz".format(time)
     except Exception:
         return None
