c48d2ccb908de91508433b355bdaf703649ab31c
humeinste <humeincreations@gmail.com>
2020-01-03 21:33:46 +0000

    working

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -9,7 +9,7 @@ def do_pack():
     try:
         local("mkdir -p versions")
         time = dateime.now().strftime("%Y%m%d%H%M%S")
-        local("tar -cvzf versions/web_static_{}.tgz".format(time))
+        local("tar -cvzf versions/web_static_{}.tgz web_static/".format(time))
         return "versions/web_static_{}.tgz".format(time)
     except Exception:
         return None
