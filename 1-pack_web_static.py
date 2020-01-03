c40aaf8ce5838eee2ecf6c4b1ee7f40edaef6ba3
humeinste <humeincreations@gmail.com>
2020-01-03 21:17:01 +0000

    check

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -8,8 +8,10 @@ from datetime import datetime
 
 def do_pack():
     """file packer"""
-    local("mkdir -p versions")
-    datetimes = datetime.now().strftime("%Y%m%d%H%M%S")
-    filepack = ("versions/web_static_().tgz").format(datetimes)
-    local("sudo tar -cvsf {} web_static".format(filepack))
-    return (file)
+    try:
+        local("mkdir -p versions")
+        datetimes = datetime.now().strftime("%Y%m%d%H%M%S")
+        filepack = ("versions/web_static_().tgz").format(datetimes)
+        local("tar -cvsf {} web_static".format(filepack))
+    except Exception:
+        return (file)
