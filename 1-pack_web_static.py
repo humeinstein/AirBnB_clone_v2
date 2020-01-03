00516be19fd9f3ed7d86d9b5c9b2b3233a58226e
humeinste <humeincreations@gmail.com>
2020-01-03 21:44:39 +0000

    working

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -7,9 +7,9 @@ from datetime import datetime
 def do_pack():
     """create tgz archive"""
     try:
-        local("mkdir -p versions")
+        local("sudo mkdir -p versions")
         time = dateime.now().strftime("%Y%m%d%H%M%S")
-        local("tar -cvzf versions/web_static_{}.tgz web_static/".format(time))
+        local("sudo tar -cvzf versions/web_static_{}.tgz web_static/".format(time))
         return "versions/web_static_{}.tgz".format(time)
     except Exception:
         return None
