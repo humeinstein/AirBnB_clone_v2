05551bd829ea6f6847b4ef2ccfaea31d83b192c6
humeinstein <humeincreations@gmail.com>
2020-01-22 17:36:41 +0000

    INIT add route

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+""" simple flask app """
+from flask import Flask as fapp
+
+
+app = fapp(__name__)
+
+
+@app.route("/")
+def sayHi():
+    """ say hi when called """
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb")
+def hbnb():
+    """ hbnb dir """
+    return "HBNB"
+
+
+@app.route("/c/<text>")
+def c_dir(text):
+    """ return C and string """
+    return ("C {}".format(text.replace('_', ' ')))
+
+if __name__ == "__main__":
+    app.run()
