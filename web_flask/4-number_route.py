9c01fdd8e4209ee8f05da78324fd5e56412ca6ed
humeinstein <humeincreations@gmail.com>
2020-01-22 20:08:40 +0000

    working

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+""" simple flask app """
+from flask import Flask as fapp
+
+
+app = fapp(__name__)
+app.url_map.strict_slashes = False
+
+@app.route("/")
+def sayHi():
+    """ say hi when called """
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb")
+def hbnb():
+    """ hbnb dir """
+    return "HBNB"
+
+
+@app.route("/c/<text>")
+def c_place(content):
+    """ return C and string """
+    return ("C {}".format(content.replace('_', ' ')))
+
+
+@app.route('/python/')
+@app.route('/python/<text>')
+def displayPython(text="is cool"):
+    """ show python followed by string """
+    if (text == "is cool"):
+        return("Python {}".format(text))
+    else:
+        return("Python {}".format(text.replace("_", " ")))
+
+@app.route('/number/<int:n>')
+def displayNumber(n):
+    """ displays number if unsigned int """
+    return("{} is a number".format(n))
+if __name__ == "__main__":
+    app.run()
